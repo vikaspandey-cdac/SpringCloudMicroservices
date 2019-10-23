25970b68dc1c09542543aeff7b39120d2d533881
Vikas Pandey <vpandey@vpandey-dt.archivas.com>
2019-10-23 17:44:46 +0530

    initial commit

---
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,11 @@
+package com.photoapp.api.users.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.photoapp.api.users.shared.UserDto;
+
+public interface UsersService extends UserDetailsService{
+	UserDto createUser(UserDto userDetails);
+	UserDto getUserDetailsByEmail(String email);
+	UserDto getUserByUserId(String userId);
+}
